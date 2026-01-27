ba3cfe49a0a3a89a5208e93308cf218b5559e4f3
Anders Logg <logg@chalmers.se>
2026-01-27 08:03:26 +0100

    Add some more solvers

---
+++ sandbox/solve_helmholtz.py
@@ -0,0 +1,145 @@
+from fenics import *
+
+# More verbose output
+set_log_level(INFO)
+
+# ------------------------------------------------------------
+# Problem parameters
+# ------------------------------------------------------------
+c = 343.0  # speed of sound (m/s)
+f = 1.0  # frequency (Hz)
+k = 2.0 * np.pi * f / c
+
+# ------------------------------------------------------------
+# Geometry
+# ------------------------------------------------------------
+# mesh, markers = load_mesh_with_markers("output/volume_mesh_gbg.xdmf")
+mesh, markers = load_mesh_with_markers("tmp/gbg_volume_mesh.xdmf")
+xmin, ymin, zmin, xmax, ymax, zmax = bounds(mesh)
+
+# ------------------------------------------------------------
+# Check if we resolve the wavelength
+# ------------------------------------------------------------
+h = mesh.hmin()
+info(f"h = {h :.3g}")
+info(f"kh = {k * h :.3g}")
+if k * h > 0.9:
+    error(f"Mesh too coarse for {f:.0f} Hz with P1 elements (k h = {k * h:.2f} > 0.9).")
+    exit(1)
+
+# ------------------------------------------------------------
+# Function space: mixed (Re, Im) → R²
+# -----------------------------------------------------------
+W = FunctionSpace(mesh, (("Lagrange", 1), ("Lagrange", 1)))
+
+# ------------------------------------------------------------
+# Source term
+# ------------------------------------------------------------
+A = 1.0  # amplitude
+sigma = 5.0  # spatial extent (m)
+x0 = np.array((0.5 * (xmin + xmax), 0.5 * (ymin + ymax), 0.9 * zmin + 0.1 * zmax))
+_x = SpatialCoordinate(mesh)
+r2 = sum((_x[i] - x0[i]) ** 2 for i in range(3))
+s = A * exp(-r2 / (2 * sigma**2))
+
+# ------------------------------------------------------------
+# Boundary conditions
+# ------------------------------------------------------------
+ds = NeumannBC(mesh, markers=markers, marker_value=[-2, -3, -4, -5, -6])
+
+# Dirichlet condition for anchor point (one dof)
+bc = DirichletBC(W.sub(0), 0.0, dofs=[0])
+# bcs = [bc] # does not seem to help much
+bcs = []
+
+# ------------------------------------------------------------
+# Variational problem
+# ------------------------------------------------------------
+(p_re, p_im) = TrialFunctions(W)
+(q_re, q_im) = TestFunctions(W)
+
+a = (
+    inner(grad(p_re), grad(q_re)) * dx
+    + inner(grad(p_im), grad(q_im)) * dx
+    - k**2 * (p_re * q_re + p_im * q_im) * dx
+    + k * (p_im * q_re - p_re * q_im) * ds
+)
+
+L = -s * q_re * dx
+
+# ------------------------------------------------------------
+# GLS stabilization (does not seem to help much)
+# ------------------------------------------------------------
+tau = 0.0
+if tau > 0.0:
+
+    h_K = CellDiameter(mesh)
+    tau = 0.1 * h_K**2
+
+    r_p_re = div(grad(p_re)) + k**2 * p_re
+    r_p_im = div(grad(p_im)) + k**2 * p_im
+    r_q_re = div(grad(q_re)) + k**2 * q_re
+    r_q_im = div(grad(q_im)) + k**2 * q_im
+
+    a += tau * (r_p_re * r_q_re + r_p_im * r_q_im) * dx
+    L += -tau * s * r_q_re * dx
+
+# ------------------------------------------------------------
+# Shifted form for preconditioning
+# ------------------------------------------------------------
+alpha = 0.2
+beta = 0.65
+diag_shift = (1.0 - alpha**2) * k**2 * (p_re * q_re + p_im * q_im) * dx
+rot_shift = beta * k**2 * (p_im * q_re - p_re * q_im) * dx
+a_pc = a + diag_shift + rot_shift
+a_pc = a
+
+# ------------------------------------------------------------
+# Linear solver
+# ------------------------------------------------------------
+direct = {
+    "ksp_monitor_short": None,
+    "ksp_converged_reason": None,
+    "ksp_type": "preonly",
+    "pc_type": "lu",
+    "pc_factor_mat_solver_type": "mumps",
+}
+
+opts = {
+    "ksp_monitor_short": None,
+    "ksp_converged_reason": None,
+    "ksp_type": "fgmres",
+    "ksp_rtol": 1.0e-6,
+    "ksp_max_it": 10000,
+    "pc_type": "hypre",
+    "pc_hypre_type": "boomeramg",
+    "pc_hypre_boomeramg_cycle_type": "W",
+    "pc_hypre_boomeramg_max_iter": 4,
+    "pc_hypre_boomeramg_coarsen_type": "HMIS",
+    "pc_hypre_boomeramg_interp_type": "ext+i",
+    "pc_hypre_boomeramg_strong_threshold": 0.5,
+    "pc_hypre_boomeramg_agg_nl": 4,
+}
+
+# Set up linear problem
+problem = LinearProblem(a, L, bcs=bcs, petsc_options=opts)
+
+# Add preconditioner
+A_pc = assemble_matrix(a_pc, bcs=bcs)
+A_pc.assemble()
+problem.solver.setOperators(problem.A, A_pc)
+
+# Solve linear problem
+p = problem.solve()
+
+# ------------------------------------------------------------
+# Post-processing & output
+# ------------------------------------------------------------
+
+# Interpolate magnitude of complex solution
+V = FunctionSpace(mesh, "Lagrange", 1)
+expr = Expression(sqrt(p[0] ** 2 + p[1] ** 2), V.element.interpolation_points())
+p_abs = interpolate(expr, V)
+
+# Save solution
+p_abs.save("output/solution_helmholtz.xdmf")
---
+++ sandbox/solve_wave.py
@@ -0,0 +1,139 @@
+from fenics import *
+
+# More verbose output
+set_log_level(INFO)
+
+# ------------------------------------------------------------
+# Problem parameters
+# ------------------------------------------------------------
+c = 343.0  # speed of sound (m/s)
+T = 1.0  # final time (s)
+skip = 10  # skip time steps when saving
+
+# ------------------------------------------------------------
+# Geometry
+# ------------------------------------------------------------
+mesh, markers = load_mesh_with_markers("output/volume_mesh_gbg.xdmf")
+xmin, ymin, zmin, xmax, ymax, zmax = bounds(mesh)
+
+# ------------------------------------------------------------
+# Set time step based on CFL condition
+# ------------------------------------------------------------
+h = mesh.hmin()
+info(f"hmin = {h :.3g}")
+dt = 0.1 * h / c  # 0.2 and above blows up
+num_steps = round(T / dt + 0.5)
+dt = T / num_steps
+info(f"Using dt = {dt :.3g} and {num_steps} time steps based on CFL condition")
+
+# ------------------------------------------------------------
+# Function space
+# ------------------------------------------------------------
+V = FunctionSpace(mesh, "Lagrange", 1)
+
+# ------------------------------------------------------------
+# Source term
+# ------------------------------------------------------------
+A = 100.0  # amplitude of source
+sigma = 5.0  # spatial extent (m)
+tau = 0.05  # temporal extent (s)
+x0 = np.array((0.5 * (xmin + xmax), 0.5 * (ymin + ymax), 0.9 * zmin + 0.1 * zmax))
+t0 = 0.1
+_x = SpatialCoordinate(mesh)
+_t = Constant(mesh, 0.0)
+r2 = sum((_x[i] - x0[i]) ** 2 for i in range(3))
+t2 = (_t - t0) ** 2
+f = A * exp(-r2 / (2 * sigma**2)) * exp(-t2 / (2 * tau**2))
+
+# ------------------------------------------------------------
+# Boundary conditions
+#
+# We use rigid boundary conditions on the ground and buildings
+# which is implemented as homogeneous Neumann = do nothing.
+#
+# We use absorbing boundary conditions on the domain bounding box
+# which is implemented as ∂_n u = - (1/c) ∂_t u (Sommerfeld).
+# ------------------------------------------------------------
+ds = NeumannBC(mesh, markers=markers, marker_value=[-2, -3, -4, -5, -6])
+
+# ------------------------------------------------------------
+# Initial conditions
+# ------------------------------------------------------------
+
+# Create functions for solution at three time levels
+u_0 = Function(V)  # u at time step n-1
+u_1 = Function(V)  # u at time step n
+u_2 = Function(V)  # u at time step n+1 (unknown to solve for)
+
+# Initial conditions (u=0, velocity=0)
+u_0.x.array[:] = 0.0
+u_1.x.array[:] = 0.0
+
+# ------------------------------------------------------------
+# Variational problem
+# ------------------------------------------------------------
+u = TrialFunction(V)
+v = TestFunction(V)
+
+alpha = Constant(mesh, c**2 * dt**2 / 6)
+beta = Constant(mesh, c * dt)
+gamma = Constant(mesh, dt**2)
+
+a = u * v * dx + alpha * inner(grad(u), grad(v)) * dx + beta * u * v * ds
+L = (2 * u_1 - u_0) * v * dx - alpha * inner(grad(4 * u_1 + u_0), grad(v)) * dx
+L += gamma * f * v * dx + beta * u_1 * v * ds
+
+# ------------------------------------------------------------
+# Linear solver
+# ------------------------------------------------------------
+direct = {
+    "ksp_monitor_short": None,
+    "ksp_converged_reason": None,
+    "ksp_type": "preonly",
+    "pc_type": "lu",
+    "pc_factor_mat_solver_type": "mumps",
+}
+
+opts = {
+    "ksp_monitor_short": None,
+    "ksp_converged_reason": None,
+    "ksp_type": "cg",
+    "ksp_rtol": 1.0e-6,
+    "pc_type": "hypre",
+    "pc_hypre_type": "boomeramg",
+}
+
+problem = LinearProblem(a, L, u=u_2, bcs=[], petsc_options=opts)
+
+# -------------------------------------------------------------
+# Time-stepping loop
+# -------------------------------------------------------------
+
+# Save initial solution
+u_2.save("output/timeseries_wave.xdmf", t=0.0)
+
+t = 0.0
+for n in range(num_steps):
+    t += dt
+    info(f"t = {t}: ||x|| = {np.linalg.norm(u_2.x.array[:])}")
+
+    # Update time for right-hand side expression
+    _t.value = t
+
+    # Solve for u_2
+    problem.solve()
+
+    # Shift solutions for next time step
+    u_0.x.array[:] = u_1.x.array
+    u_1.x.array[:] = u_2.x.array
+
+    # Save solution
+    if n % skip == 0:
+        u_2.save("output/timeseries_wave.xdmf", t=t)
+
+# ------------------------------------------------------------
+# Post-processing & output
+# ------------------------------------------------------------
+
+# Save final solution
+u_2.save("output/solution_wave.xdmf")
