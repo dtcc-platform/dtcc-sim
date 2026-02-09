7c64a36834baaddb370398c34807436efd454d12
Anders Logg <logg@chalmers.se>
2026-02-09 18:29:26 +0100

    Add flat city mesh demo

--- sandbox/build_flat_mesh_gbg.py
+++ sandbox/build_flat_mesh_gbg.py
@@ -1,4 +1,4 @@
-# This script builds a tetrahedral volume mesh for an area in Gothenburg.
+# This script builds a flat 2D mesh for an area in Gothenburg.
 
 from dtcc import datasets, Bounds
 
@@ -8,17 +8,16 @@ y0 = 6399009.716755
 
 # Meshing parameters
 h = 25.0  # max mesh size
-H = 80.0  # domain height
 L = 400.0  # domain size
 
 # Define bounds
 bounds = Bounds(x0 - 0.5 * L, y0 - 0.5 * L, x0 + 0.5 * L, y0 + 0.5 * L)
 
-# Get volume mesh dataset
-volume_mesh = datasets.city_volume_mesh(bounds=bounds, max_mesh_size=h, domain_height=H)
+# Get flat mesh dataset
+flat_mesh = datasets.city_flat_mesh(bounds=bounds, max_mesh_size=h)
 
 # Save to file
-volume_mesh.save("output/volume_mesh_gbg.xdmf")
+flat_mesh.save("output/flat_mesh_gbg.xdmf")
 
 # View mesh
-# volume_mesh.view()
+# flat_mesh.view()
